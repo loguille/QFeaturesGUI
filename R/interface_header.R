7880653c3ece1e81dbb8d83138a0692ce8ca898b
loguille <loic.guille@uclouvain.be>
2026-05-04 09:30:06 +0200

    add link to documentation in the header

--- R/interface_header.R
+++ R/interface_header.R
@@ -6,12 +6,22 @@
 #' @rdname INTERNAL_interface_header
 #' @keywords internal
 #'
-#' @importFrom shinydashboard dropdownMenuOutput
-#' @importFrom shinydashboardPlus dashboardHeader
+#' @importFrom shinydashboard dropdownMenuOutput dropdownMenu
+#' @importFrom shinydashboardPlus dashboardHeader notificationItem
 #'
 header <- function(title) {
     dashboardHeader(
         title = title,
-        dropdownMenuOutput("exception_menu")
+        dropdownMenuOutput("exception_menu"),
+        dropdownMenu(
+          type = "notifications",
+          badgeStatus = NULL,
+          icon = icon("circle-question"),
+          shinydashboardPlus::notificationItem(
+            text = "Online Documentation",
+            icon = icon("arrow-up-right-from-square"),
+            status = "success",
+            href = "https://uclouvain-cbio.github.io/QFeaturesGUI/")
+        )
     )
 }
