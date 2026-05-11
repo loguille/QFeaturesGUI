0b0492694f8aa507809077b304c9de16de963d6c
loguille <loic.guille@uclouvain.be>
2026-05-11 14:38:07 +0200

    Add tooltip on filtering page

--- R/interface_module_filtering_tab.R
+++ R/interface_module_filtering_tab.R
@@ -97,22 +97,5 @@ interface_module_filtering_tab <- function(id, type = c("samples", "features")) 
             ),
             placement = "top"
         )
-        # actionButton(
-        #     NS(id, "export"),
-        #     "Save the processed sets",
-        #     icon("hand-pointer", class = "fa-solid"),
-        #     width = "100%",
-        #     class = "load-button"
-        # ),
-        # 
-        # shinyBS::bsTooltip(
-        #     id = NS(id, "export"),
-        #     title = paste("Write the processed sets to the QFeatures object.",
-        #         "This is needed to proceed to the next steps.",
-        #         sep = " "
-        #     ),
-        #     trigger = "hover",
-        #     placement = "top"
-        # )
     )
 }
