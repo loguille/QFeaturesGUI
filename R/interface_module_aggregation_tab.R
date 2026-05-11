2d7e04d0ac2007ff382f10e5631280fdf1d42709
loguille <loic.guille@uclouvain.be>
2026-05-11 14:52:17 +0200

    Add tooltip for aggregation step

--- R/interface_module_aggregation_tab.R
+++ R/interface_module_aggregation_tab.R
@@ -20,7 +20,10 @@ interface_module_aggregation_tab <- function(id) {
                 collapsible = TRUE,
                 selectInput(
                     inputId = NS(id, "method"),
-                    label = "function to aggregate",
+                    label = bs3Tooltip(
+                        trigger = "function to aggregate",
+                        tooltipText = "See online documentation for more information about methods."
+                    ),
                     choices = c(
                         "robustSummary",
                         "medianPolish",
@@ -33,7 +36,10 @@ interface_module_aggregation_tab <- function(id) {
                 br(),
                 selectInput(
                     inputId = NS(id, "fcol"),
-                    "rowData variable defining the features of the assay to aggregate",
+                    bs3Tooltip(
+                        trigger = "rowData variable defining the features of the assay to aggregate",
+                        tooltipText = "Metadata of the assay to aggregate (not sure)."
+                    ),
                     choices = NULL
                 ),
                 br(),
@@ -74,22 +80,21 @@ interface_module_aggregation_tab <- function(id) {
                 uiOutput(NS(id, "aggregation_boxplot_ui"))
             )
         ),
-        actionButton(
-            NS(id, "export"),
-            "Save the processed sets",
-            icon("hand-pointer", class = "fa-solid"),
-            width = "100%",
-            class = "load-button"
-        ),
-        shinyBS::bsTooltip(
-            id = NS(id, "export"),
-            title = paste("Write the processed sets to the QFeatures object.",
+         bs3Tooltip(
+            trigger = shiny::actionButton(
+                inputId = NS(id, "export"),
+                "Save the processed sets",
+                icon("hand-pointer", class = "fa-solid"),
+                width = "100%",
+                class = "load-button"
+            ),
+            tooltipText = paste(
+                "Write the processed sets to the QFeatures object.",
                 "This is needed to proceed to the next steps.",
                 sep = " "
             ),
-            trigger = "hover",
             placement = "top"
-        )
+         )
     )
 }
 
