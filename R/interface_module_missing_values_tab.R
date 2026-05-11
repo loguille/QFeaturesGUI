e825774ef523d85adb84a7417741a44592274068
loguille <loic.guille@uclouvain.be>
2026-05-11 15:29:33 +0200

    Remove shinyBS for missing values page

--- R/interface_module_missing_values_tab.R
+++ R/interface_module_missing_values_tab.R
@@ -8,7 +8,6 @@
 #' @importFrom shinydashboardPlus box boxSidebar
 #' @importFrom shinydashboard infoBoxOutput
 #' @importFrom htmltools tagList
-#' @importFrom shinyBS bsTooltip
 #'
 interface_module_missing_values_tab <- function(id, type) {
     tagList(
@@ -55,20 +54,19 @@ interface_module_missing_values_tab <- function(id, type) {
             ),
             uiOutput(NS(id, paste0("dynamic_", type)))
         ),
-        actionButton(
-            NS(id, "export"),
-            "Save the processed sets",
-            icon("hand-pointer", class = "fa-solid"),
-            width = "100%",
-            class = "load-button"
-        ),
-        shinyBS::bsTooltip(
-            id = NS(id, "export"),
-            title = paste("Write the processed sets to the QFeatures object.",
+        bs3Tooltip(
+            actionButton(
+                NS(id, "export"),
+                "Save the processed sets",
+                icon("hand-pointer", class = "fa-solid"),
+                width = "100%",
+                class = "load-button"
+            ),
+            paste(
+                "Write the processed sets to the QFeatures object.",
                 "This is needed to proceed to the next steps.",
                 sep = " "
             ),
-            trigger = "hover",
             placement = "top"
         )
     )
